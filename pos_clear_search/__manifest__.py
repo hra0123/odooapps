ffdabde8ae99ee38de6bc7b71491d985e4d1abc8
Muhammed Aslam <aslam@catalistindia.in>
2022-07-07 17:30:53 +0530

    banner and license are added

--- pos_clear_search/__manifest__.py
+++ pos_clear_search/__manifest__.py
@@ -8,11 +8,13 @@
 	'description':""" This module is used to clear items from search 
 	""",
 	'author':"Muhammed Aslam",
+	'license': 'OPL-1',
 	'website':'https://www.linkedin.com/in/muhammed-aslam-817327106/',
 	'depends':['point_of_sale'],
 	'data':[
 		'views/assets.xml',
 	],
+	'images': ['static/description/banner.png'],
 	'qweb': [
 		'static/src/xml/clear_input_search.xml'
 	],
